cf62995f4073a8fc9a6f42be886f2f3118dc89fa
Juan Esteban osorio Lopera <juan.osorio@sofka.com.co>
2025-02-10 14:10:00 -0500

    se inicia curso de springboot reactive

---
+++ src/main/java/com/example/springboot_reactor/app/SpringbootReactorApplication.java
@@ -0,0 +1,42 @@
+package com.example.springboot_reactor.app;
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class SpringbootReactorApplication implements CommandLineRunner {
+
+
+
+	private static final Logger log = LoggerFactory.getLogger(SpringbootReactorApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> nombres= Flux.just("andres","pedro","","diego","matias")
+				.doOnNext(e -> {
+					if(e.isEmpty()) {
+						throw new RuntimeException("El nombre no puede ser vacio");
+					}{System.out.println(e);
+					}
+				});
+
+		nombres.subscribe(e -> log.info(e),
+				error -> log.error(error.getMessage()),
+				new Runnable() {
+					@Override
+					public void run() {
+						log.info("Ha finalizado la eejcucion del observable con exito");
+					}
+				}
+		);
+	}
+}
