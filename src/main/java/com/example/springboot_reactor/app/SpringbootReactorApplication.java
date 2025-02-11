d9e652036d43b24dff08e116501970580f1dcd71
Juan Esteban osorio Lopera <juan.osorio@sofka.com.co>
2025-02-10 20:35:30 -0500

    implementando el map

--- src/main/java/com/example/springboot_reactor/app/SpringbootReactorApplication.java
+++ src/main/java/com/example/springboot_reactor/app/SpringbootReactorApplication.java
@@ -1,7 +1,6 @@
 package com.example.springboot_reactor.app;
 
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -13,30 +12,64 @@ import reactor.core.publisher.Flux;
 public class SpringbootReactorApplication implements CommandLineRunner {
 
 
+    private static final Logger log = LoggerFactory.getLogger(SpringbootReactorApplication.class);
 
-	private static final Logger log = LoggerFactory.getLogger(SpringbootReactorApplication.class);
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootReactorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootReactorApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		Flux<String> nombres= Flux.just("andres","pedro","","diego","matias")
-				.doOnNext(e -> {
-					if(e.isEmpty()) {
-						throw new RuntimeException("El nombre no puede ser vacio");
-					}{System.out.println(e);
-					}
-				});
+    @Override
+    public void run(String... args) throws Exception {
 
-		nombres.subscribe(e -> log.info(e),
-				error -> log.error(error.getMessage()),
-				new Runnable() {
-					@Override
-					public void run() {
-						log.info("Ha finalizado la eejcucion del observable con exito");
-					}
-				}
-		);
-	}
+        //  ejecutarPrimerSegmento();
+
+        agregando_el_map();
+    }
+
+    private void agregando_el_map() {
+        Flux<String> nombres = Flux.just("andres", "pedro", "juanes", "diego", "matias")
+                .map(nombre -> {
+                    return nombre.toUpperCase();
+                })
+
+                .doOnNext(e -> {
+                    if (e.isEmpty()) {
+                        throw new RuntimeException("El nombre no puede ser vacio");
+                    }
+                    System.out.println(e);
+                })
+
+                .map(nombre -> {
+                    return nombre.toLowerCase();
+                });
+
+        nombres.subscribe(
+                e -> log.info(e),
+                error -> log.error(error.getMessage()),
+                () -> log.info("Ha finalizado la ejecución del observable con éxito")
+        );
+    }
+
+
+
+
+
+    private void ejecutarPrimerSegmento() {
+        Flux<String> nombres = Flux.just("andres", "pedro", "juanes", "diego", "matias")
+                .doOnNext(e -> {
+                    if (e.isEmpty()) {
+                        throw new RuntimeException("El nombre no puede ser vacio");
+                    }
+                    System.out.println(e);
+                });
+
+        nombres.subscribe(
+                e -> log.info(e),
+                error -> log.error(error.getMessage()),
+                () -> log.info("Ha finalizado la ejecución del observable con éxito")
+        );
+    }
+
+
+
 }
