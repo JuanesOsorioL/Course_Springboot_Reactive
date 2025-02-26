fe96b59184ffbcab518bfcfdec8551142e006358
Juan Esteban osorio Lopera <juan.osorio@sofka.com.co>
2025-02-26 11:23:55 -0500

    implementando el flatmap como filter

--- src/main/java/com/example/springboot_reactor/app/SpringbootReactorApplication.java
+++ src/main/java/com/example/springboot_reactor/app/SpringbootReactorApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,7 +29,40 @@ public class SpringbootReactorApplication implements CommandLineRunner {
         // agregando_el_map_simple();
         // agregando_el_map_a_modelo();
         // agregando_el_map_con_el_operador_filter();
-        ejemplo_map_filter_consumiendo_una_lista();
+        // ejemplo_map_filter_consumiendo_una_lista();
+        ejemplo_flapmap();
+    }
+
+
+
+    private void ejemplo_flapmap() {
+        List<String> usuariosList = new ArrayList<>();
+        usuariosList.add("andres lopez");
+        usuariosList.add("pedro santamaria");
+        usuariosList.add("juan osorio");
+        usuariosList.add("diego lopera");
+        usuariosList.add("matias avendaño");
+        usuariosList.add("lukas bueno");
+        usuariosList.add("valeria lopez");
+        usuariosList.add("juan lopera");
+
+        Flux.fromIterable(usuariosList)
+                .map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
+                .flatMap(usuario -> {
+                    if (usuario.getNombre().equalsIgnoreCase("juan")) {
+                        return Mono.just(usuario);
+                    }else {
+                        return Mono.empty();
+                    }
+
+                })
+                .map(usuario -> {
+                    String nombre = usuario.getNombre().toLowerCase();
+                    usuario.setNombre(nombre);
+                    return usuario;
+                })
+
+        .subscribe(e -> log.info(e.toString()));
     }
 
     private void ejemplo_map_filter_consumiendo_una_lista() {
@@ -64,7 +98,6 @@ public class SpringbootReactorApplication implements CommandLineRunner {
                 () -> log.info("Ha finalizado la ejecución del observable con éxito")
         );
     }
-
 
     private void agregando_el_map_con_el_operador_filter() {
         Flux<Usuario> nombres = Flux.just("andres lopez", "pedro santamaria", "juan osorio", "diego lopera", "matias avendaño", "lukas bueno", "valeria lopez", "juan lopera")
